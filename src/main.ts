d0003e90f43bd8086ac8ba06e56ac7fdf4a04ef8
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-04-21 13:27:51 +0200

    codemirror as external

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,52 @@
 import { App, Plugin } from 'obsidian';
 import type { TemplateSettings } from './interfaces';
-import TemplateSettingTab from './settings';
+//import TemplateSettingTab from './settings';
+import {criticmarkupLanguage} from 'lang-criticmarkup'
 
 const DEFAULT_SETTINGS: TemplateSettings = {};
 
 export default class TemplatePlugin extends Plugin {
-	//@ts-ignore
-	settings: TemplateSettings;
+	//settings: TemplateSettings;
 
 	async onload() {
 		console.log('loading ... plugin');
 
+/*
 		await this.loadSettings();
 
 		this.addSettingTab(new TemplateSettingTab(this.app, this));
+		const ext = this.buildCMPlugin();
+		this.registerEditorExtension(ext)
 	}
+
+	buildCMPlugin() {
+		const viewPlugin = viewPlugin.fromClass(
+			class {}
+		)
+		return viewPlugin
+*/
+
+		this.registerEvent(
+			this.app.workspace.on('active-leaf-change', (leaf) => {
+				if (leaf && (leaf.getViewState().type === 'markdown')) {
+					const content = leaf.view.editor.getValue() as string
+					const tree = criticmarkupLanguage.parser.parse(content)
+					console.log(tree)
+					const cursor = tree.cursor()
+					do {
+						console.log(`Node ${cursor.name} from ${cursor.from} to ${cursor.to}`)
+					} while (cursor.next())
+				}
+			})
+		)
+	}
+
 
 	onunload() {
 		console.log('unloading ... plugin');
 	}
 
+/*
 	async loadSettings() {
 		this.settings = Object.assign(
 			{},
@@ -31,4 +58,5 @@ export default class TemplatePlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+*/
 }
