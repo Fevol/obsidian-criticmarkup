db6f6cbe794f42eff825d03e6ab79424054f82ac
Fevol <vernieuwefelix@gmail.com>
2023-03-05 17:41:07 +0100

    feat: added replace in selection command and context menu

---
+++ src/editor/context-menu-commands.ts
@@ -0,0 +1,33 @@
+import type { EventRef } from 'obsidian';
+import { acceptAllSuggestions, rejectAllSuggestions } from './commands';
+import { selectionToRange } from './util';
+
+
+export const change_suggestions: EventRef =
+	app.workspace.on('editor-menu', (menu, editor) => {
+		menu.addItem((item) => {
+			item.setTitle('Accept changes')
+				.setIcon('check')
+				.setSection('criticmarkup')
+				.onClick(() => {
+					const [from, to] = selectionToRange(editor);
+					// @ts-ignore (editor.cm.dispatch exists)
+					editor.cm.dispatch(editor.cm.state.update({
+						changes: acceptAllSuggestions(editor.getValue(), from, to),
+					}));
+				});
+		});
+
+		menu.addItem((item) => {
+			item.setTitle('Reject changes')
+				.setIcon('cross')
+				.setSection('criticmarkup')
+				.onClick(() => {
+					const [from, to] = selectionToRange(editor);
+					// @ts-ignore (editor.cm.dispatch exists)
+					editor.cm.dispatch(editor.cm.state.update({
+						changes: rejectAllSuggestions(editor.getValue(), from, to),
+					}));
+				});
+		});
+	});
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { inlinePlugin } from './editor/live-preview';
 import { postProcess } from './editor/post-processor';
 
 import {commands} from './editor/commands';
+import { change_suggestions } from './editor/context-menu-commands';
 
 export default class CriticMarkupPlugin extends Plugin {
 
 	async onload() {
 		this.registerEditorExtension([inlinePlugin()]);
 		this.registerMarkdownPostProcessor((el, ctx) => postProcess(el, ctx));
-
+		this.registerEvent(change_suggestions);
 
 		for (const command of commands) {
 			if (Platform.isMobile || command.editor_context) {
