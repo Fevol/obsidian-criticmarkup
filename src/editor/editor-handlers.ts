0477532a98d9dcd2576ab6e4030b5b36c7d303d4
Fevol <vernieuwefelix@gmail.com>
2023-08-29 00:32:50 +0200

    chore: refactor nodeCorrecter function to use new nodes system

--- src/editor/editor-handlers.ts
+++ src/editor/editor-handlers.ts
@@ -4,7 +4,8 @@ import { type ChangeSpec, EditorSelection, EditorState, Prec } from '@codemirror
 import { treeParser } from './tree-parser';
 
 import { CM_Brackets } from '../util';
-import { moveEditorCursor, nodeAtCursor } from './editor-util';
+import { nodesInSelection } from './editor-util';
+import { NodeType } from '../types';
 
 export const bracketMatcher = Prec.high(EditorView.inputHandler.of((view, from, to, text) => {
 	const before = view.state.doc.sliceString(from - 2, from) + text;
@@ -26,32 +27,35 @@ export const bracketMatcher = Prec.high(EditorView.inputHandler.of((view, from, 
 	return false;
 }));
 
+/**
+ * Removes initial whitespaces and double newlines from nodes that would otherwise result in markup being applied
+ * to text that is not part of the node (due to CM shenanigans)
+ */
 export const nodeCorrecter = EditorState.transactionFilter.of(tr => {
-	// From 'tr', get the spec from a decoration transaction
 	if (tr.isUserEvent('select')) {
-		const previous_selection = <EditorSelection>tr.startState.selection,
-			current_selection = <EditorSelection>tr.selection;
+		const previous_selection = tr.startState.selection.main, current_selection = tr.selection!.main;
 
-		if (current_selection.main.anchor === current_selection.main.head) {
+		if (current_selection.anchor === current_selection.head) {
 			const text = tr.startState.doc.toString();
 			const tree = tr.startState.field(treeParser).tree;
+			const nodes = nodesInSelection(tree)
 
-			const start_node = nodeAtCursor(tree, previous_selection.main.head);
-			const end_node = nodeAtCursor(tree, current_selection.main.head);
+			const start_node = nodes.at_cursor(previous_selection.head);
+			const end_node = nodes.at_cursor(current_selection.head);
 
+			// Execute only if the cursor is moved outside a particular node
 			if (start_node && start_node !== end_node &&
-				(start_node?.type.name === 'Substitution' || start_node?.type.name === 'Highlight')) {
-				let new_text = text.slice(start_node.from, start_node.to);
+				(start_node.type === NodeType.SUBSTITUTION || start_node.type === NodeType.HIGHLIGHT)) {
+				let new_text = start_node.unwrap(text);
 				let changed = false;
 
 				let removed_characters = 0;
-				let left_whitespace_end = new_text.slice(3).search(/\S/);
+				const left_whitespace_end = new_text.search(/\S/);
 				if (left_whitespace_end >= 1) {
 					changed = true;
-					new_text = new_text.slice(0, 3) + new_text.slice(3 + left_whitespace_end);
+					new_text = new_text.slice(left_whitespace_end);
 					removed_characters += left_whitespace_end;
-				} else
-					left_whitespace_end = 0;
+				}
 
 				const invalid_endlines = new_text.match(/\n\s*\n/g);
 				if (invalid_endlines) {
@@ -62,13 +66,13 @@ export const nodeCorrecter = EditorState.transactionFilter.of(tr => {
 
 				if (changed) {
 					const changes: ChangeSpec[] = [{
-						from: start_node.from,
-						to: start_node.to,
+						from: start_node.from + 3,
+						to: start_node.to - 3,
 						insert: new_text,
 					}];
 					return {
 						changes,
-						selection: moveEditorCursor(current_selection, start_node.from + 3 + left_whitespace_end, -removed_characters),
+						selection: EditorSelection.cursor(current_selection.head - removed_characters),
 					};
 				}
 			}
--- src/editor/editor-util.ts
+++ src/editor/editor-util.ts
@@ -1,159 +1,136 @@
-import type { Editor, EditorPosition } from 'obsidian';
-
 import type { Tree } from '@lezer/common';
 import {
-	ChangeSet,
-	CharCategory,
-	EditorSelection,
-	EditorState,
-	findClusterBreak,
-	Transaction,
-	Text, SelectionRange,
+    ChangeSet,
+    CharCategory,
+    EditorSelection,
+    EditorState,
+    findClusterBreak,
+    Transaction,
+    Text, SelectionRange,
 } from '@codemirror/state';
 import { constructNode, CriticMarkupNode, CriticMarkupNodes } from './criticmarkup-nodes';
 import { type CriticMarkupOperation } from '../types';
 
 
-export function moveEditorCursor(selection: EditorSelection, change_start: number, offset: number) {
-	if (change_start >= selection.ranges[0].from)
-		return selection;
-	return EditorSelection.range(
-		selection.ranges[0].from + offset,
-		selection.ranges[0].to + offset,
-	);
-}
-
-
 export function selectionRangeOverlap(selection: EditorSelection, rangeFrom: number, rangeTo: number) {
-	return selection.ranges.some(range => range.from <= rangeTo && range.to >= rangeFrom);
-}
-
-
-export function nodeAtCursor(tree: Tree, pos: number) {
-	const node = tree.resolve(pos, -1);
-	if (node.type.name === '⚠' || node.type.name === 'CriticMarkup')
-		return undefined;
-	if (node.type.name === 'MSub')
-		return node.parent;
-	return node;
+    return selection.ranges.some(range => range.from <= rangeTo && range.to >= rangeFrom);
 }
 
 
 export function nodesInSelection(tree: Tree, start?: number, end?: number) {
-	const nodes: CriticMarkupNode[] = [];
+    const nodes: CriticMarkupNode[] = [];
 
-	tree.iterate({
-		from: start,
-		to: end,
-		enter: (node) => {
-			// FIXME: Add check here whether [node.to - 3, node.to] is in fact a bracket, prevent half-open nodes from actually being considered as nodes
-			if (node.type.name === '⚠')
-				return false;
-			if (node.type.name === 'CriticMarkup' || node.type.name === 'MSub')
-				return;
-			if (node.type.name === 'Substitution') {
-				if (node.node.firstChild?.type.name !== 'MSub')
-					return;
-				nodes.push(constructNode(node.from, node.to, node.type.name, node.node.firstChild?.from)!);
-			} else {
-				nodes.push(constructNode(node.from, node.to, node.type.name, node.node.firstChild?.from)!);
-			}
-		},
-	});
-	return new CriticMarkupNodes(nodes);
+    tree.iterate({
+        from: start,
+        to: end,
+        enter: (node) => {
+            // FIXME: Add check here whether [node.to - 3, node.to] is in fact a bracket, prevent half-open nodes from actually being considered as nodes
+            if (node.type.name === '⚠')
+                return false;
+            if (node.type.name === 'CriticMarkup' || node.type.name === 'MSub')
+                return;
+            if (node.type.name === 'Substitution') {
+                if (node.node.firstChild?.type.name !== 'MSub')
+                    return;
+                nodes.push(constructNode(node.from, node.to, node.type.name, node.node.firstChild?.from)!);
+            } else {
+                nodes.push(constructNode(node.from, node.to, node.type.name, node.node.firstChild?.from)!);
+            }
+        },
+    });
+    return new CriticMarkupNodes(nodes);
 }
-
 
 
 export function findBlockingChar(start: number, forward: boolean, state: EditorState, ignore_initial_space = false, cat: CharCategory | null = null): [number, number | null] {
-	let pos = start;
-	const line = state.doc.lineAt(pos);
-	// 0: Word		1: Space		2: Other
-	const categorize = state.charCategorizer(pos)
-	if (cat === CharCategory.Space) cat = null;
+    let pos = start;
+    const line = state.doc.lineAt(pos);
+    // 0: Word		1: Space		2: Other
+    const categorize = state.charCategorizer(pos);
+    if (cat === CharCategory.Space) cat = null;
 
-	let nextCat = null
-	for (cat;;) {
-		if (pos == (forward ? line.to : line.from)) {
-			if (pos == start && line.number != (forward ? state.doc.lines : 1))
-				pos += forward ? 1 : -1
-			break
-		}
-		const next = findClusterBreak(line.text, pos - line.from, forward) + line.from
-		const nextChar = line.text.slice(Math.min(pos, next) - line.from, Math.max(pos, next) - line.from)
-		nextCat = categorize(nextChar)
-		if (cat != null && nextCat != cat) {
-			if (cat == 1 && ignore_initial_space) ignore_initial_space = false
-			else break
-		}
-		if (nextChar != " " || pos != start) cat = nextCat
-		pos = next
-	}
-	return [pos, cat]
+    let nextCat = null;
+    for (cat; ;) {
+        if (pos == (forward ? line.to : line.from)) {
+            if (pos == start && line.number != (forward ? state.doc.lines : 1))
+                pos += forward ? 1 : -1;
+            break;
+        }
+        const next = findClusterBreak(line.text, pos - line.from, forward) + line.from;
+        const nextChar = line.text.slice(Math.min(pos, next) - line.from, Math.max(pos, next) - line.from);
+        nextCat = categorize(nextChar);
+        if (cat != null && nextCat != cat) {
+            if (cat == 1 && ignore_initial_space) ignore_initial_space = false;
+            else break;
+        }
+        if (nextChar != ' ' || pos != start) cat = nextCat;
+        pos = next;
+    }
+    return [pos, cat];
 }
 
 export function isBlockingChar(pos: number, state: EditorState) {
-	return !getCharCategory(pos, state);
+    return !getCharCategory(pos, state);
 }
 
 export function getCharCategory(pos: number, state: EditorState, left: boolean = false) {
-	const line = state.doc.lineAt(pos);
-	const categorize = state.charCategorizer(pos)
+    const line = state.doc.lineAt(pos);
+    const categorize = state.charCategorizer(pos);
 
-	if (left)
-		pos -= 1;
+    if (left)
+        pos -= 1;
 
-	// Categorize character at pos, if categorize(char) === 0, then it is not a blocking character
-	return categorize(line.text.slice(pos - line.from, pos - line.from + 1));
+    // Categorize character at pos, if categorize(char) === 0, then it is not a blocking character
+    return categorize(line.text.slice(pos - line.from, pos - line.from + 1));
 }
 
 
 export function getUserEvents(tr: Transaction) {
-	//@ts-ignore (Transaction has annotations)
-	return tr.annotations.map(x => x.value).filter(x => typeof x === 'string');
+    //@ts-ignore (Transaction has annotations)
+    return tr.annotations.map(x => x.value).filter(x => typeof x === 'string');
 }
 
 export function cursorMoved(tr: Transaction) {
-	return tr.startState.selection.ranges[0].from !== tr.selection!.ranges[0].from || tr.startState.selection.ranges[0].to !== tr.selection!.ranges[0].to;
+    return tr.startState.selection.ranges[0].from !== tr.selection!.ranges[0].from || tr.startState.selection.ranges[0].to !== tr.selection!.ranges[0].to;
 }
 
 export function getEditorRanges(changes: ChangeSet, doc: Text): CriticMarkupOperation[] {
-	const changed_ranges: CriticMarkupOperation[] = [];
-	changes.iterChanges((fromA, toA, fromB, toB, inserted) => {
-		let text = '';
-		// @ts-ignore (Inserted always exists when iterating changes)
-		if (inserted.text.length === 1 && inserted.text[0] === '')
-			text += '';
-		else {
-			// @ts-ignore (text exists on Text in inserted)
-			const change_text = inserted.text.join('');
-			text += change_text.length ? change_text : '\n';
-		}
+    const changed_ranges: CriticMarkupOperation[] = [];
+    changes.iterChanges((fromA, toA, fromB, toB, inserted) => {
+        let text = '';
+        // @ts-ignore (Inserted always exists when iterating changes)
+        if (inserted.text.length === 1 && inserted.text[0] === '')
+            text += '';
+        else {
+            // @ts-ignore (text exists on Text in inserted)
+            const change_text = inserted.text.join('');
+            text += change_text.length ? change_text : '\n';
+        }
 
-		changed_ranges.push({
-			from: fromA,
-			to: toA,
-			offset: {
-				removed: toA - fromA,
-				added: toB - fromB,
-			},
-			inserted: text,
-			deleted: toA - fromA ? doc.sliceString(fromA, toA) : '',
-		});
-	});
+        changed_ranges.push({
+            from: fromA,
+            to: toA,
+            offset: {
+                removed: toA - fromA,
+                added: toB - fromB,
+            },
+            inserted: text,
+            deleted: toA - fromA ? doc.sliceString(fromA, toA) : '',
+        });
+    });
 
-	return changed_ranges;
+    return changed_ranges;
 }
 
 export function selectionToEditorRange(selection: SelectionRange, text: Text, isDelete = false): CriticMarkupOperation {
-	return {
-		from: selection.from,
-		to: selection.to,
-		offset: {
-			removed: isDelete ? selection.to - selection.from : 0,
-			added: 0,
-		},
-		inserted: '',
-		deleted: isDelete ? text.sliceString(selection.from, selection.to) : '',
-	};
+    return {
+        from: selection.from,
+        to: selection.to,
+        offset: {
+            removed: isDelete ? selection.to - selection.from : 0,
+            added: 0,
+        },
+        inserted: '',
+        deleted: isDelete ? text.sliceString(selection.from, selection.to) : '',
+    };
 }
